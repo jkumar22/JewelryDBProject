f3bbe09b637b5f29c7dc2715f83114ae093214a2
vishvadha <vkari@umich.edu>
2018-11-29 17:43:55 -0500

    images

--- Pages/Customizebracelet.php
+++ Pages/Customizebracelet.php
@@ -222,7 +222,7 @@
             <p><b>Price: 127$</b></p>
             <button type="button" name="Add to Cart" onClick="document.location.href='Cart.php';"class="Add to Cart">Add to Cart</button>
         </div>
-    </div>
+    
 
         <div class="container">
         <div id="btnContainer">
@@ -303,9 +303,9 @@
                 <p><b>Price: 187$</b></p>
                 <button type="button" name="Add to Cart" onClick="document.location.href='Cart.php';"class="Add to Cart">Add to Cart</button>
             </div>
-        </div>
 
-        <div>
+
+
            <div class="container">
             <div id="btnContainer">
             </div>
