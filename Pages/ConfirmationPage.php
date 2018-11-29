18dedbb4ca1dfcdb4a1342f17c597decceda1a3b
Deekshita <sdeekshi@umich.edu>
2018-11-29 13:32:01 -0500

    update1

--- Pages/ConfirmationPage.php
+++ Pages/ConfirmationPage.php
@@ -9,8 +9,8 @@
 <form class="modal-content" style="width:50%">
     <div class="container">
         <h2>Thank you for Registering to HelloWorld </h2>
-        <p>You have been added to list of people with awesomess.</p>
-        <p>Have a great journy!</p>
+        <p>You have been added to the list of awesome people.</p>
+        <p>Have a happy shopping experience!</p>
         <hr>
         <div class="clearfix" style="width:40%">
             <Button type="button" class="signupbtn" onClick="document.location.href='index.php';"/>Register Again!</Button>
--- Process/LogOut_Process.php
+++ Process/LogOut_Process.php
@@ -6,7 +6,7 @@ $_SESSION['loggedIn'] = "False";
 $_SESSION['adminFlag'] = 0;
 $_SESSION['userID'] = "";
 
-$message = "You have logged out.\\nCOME BACK SOON!!";
+$message = "You have now logged out.\\nCOME BACK SOON!!";
 echo "<script type='text/javascript'>alert('$message');</script>";
 
 header('Location: ../Pages/index.php');
